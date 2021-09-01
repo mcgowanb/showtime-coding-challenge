1d6f5e2dea6094e7ea6ade34a382b148ae8f488e
Brian McGowan <mcgowan.b@gmail.com>
2021-09-01 23:31:51 +0100

    adding validation

--- src/main/java/com/showtime/analytics/codingchallenge/controller/UrlController.java
+++ src/main/java/com/showtime/analytics/codingchallenge/controller/UrlController.java
@@ -1,9 +1,11 @@
 package com.showtime.analytics.codingchallenge.controller;
 
 import static com.showtime.analytics.codingchallenge.constant.ApplicationConstants.API_PATH_DECODE;
+import static com.showtime.analytics.codingchallenge.constant.ApplicationConstants.API_PATH_ENCODE;
 import static com.showtime.analytics.codingchallenge.constant.ApplicationConstants.API_URL_PATH;
 import static com.showtime.analytics.codingchallenge.constant.ApplicationConstants.PATH_PARAM_URL;
 
+import javax.validation.Valid;
 import javax.websocket.server.PathParam;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,8 +15,12 @@ import lombok.extern.log4j.Log4j2;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.showtime.analytics.codingchallenge.dto.UrlDto;
 
 @Log4j2
 @RestController
@@ -23,25 +29,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UrlController {
 
-//  @Operation(summary = "Get Shortened URL",
-//      description = "Get the shortend url for the fully qualified web address",
-//      responses = {
-//          @ApiResponse(responseCode = "200", description = "Success"),
-//          @ApiResponse(responseCode = "500", description = "Internal Server Error")
-//      })
-//  @GetMapping("/{" + PATH_PARAM_FQDN + "}")
-//  ResponseEntity<String> getShortenedUrl(@PathVariable(name = PATH_PARAM_FQDN) final String fqdn) {
-//    return ResponseEntity.ok("Hello world, url is: " + fqdn);
-//  }
-
-  @Operation(summary = "Get Shortened URL",
-      description = "Get the shortend url for the fully qualified web address",
+  @Operation(summary = "Get shortened URL",
+      description = "Take a shortened url and return a redirect to the fqdn of the url",
       responses = {
-          @ApiResponse(responseCode = "200", description = "Success"),
-          @ApiResponse(responseCode = "500", description = "Internal Server Error")
+          @ApiResponse(responseCode = "301", description = "Successful match & redirect"),
+          @ApiResponse(responseCode = "500", description = "Internal Server Error"),
+          @ApiResponse(responseCode = "400", description = "Target url is invalid and no longer available"),
+          @ApiResponse(responseCode = "404", description = "URL does not exist in our database")
       })
   @GetMapping(API_PATH_DECODE)
-  ResponseEntity<String> getShortenedUrl(@PathParam(PATH_PARAM_URL) final String url) {
+  ResponseEntity<String> getUrlRedirect(@PathParam(PATH_PARAM_URL) final String url) {
     return ResponseEntity.ok("Hello world, url is: " + url);
+  }
+
+  @Operation(summary = "Create a shortened URL",
+      description = "Take a url and return a shortened url.",
+      responses = {
+          @ApiResponse(responseCode = "201", description = "Successful created"),
+          @ApiResponse(responseCode = "500", description = "Internal Server Error"),
+          @ApiResponse(responseCode = "400", description = "Target url is invalid and no longer available"),
+          @ApiResponse(responseCode = "404", description = "URL does not exist in our database")
+      })
+  @PostMapping(API_PATH_ENCODE)
+  ResponseEntity<UrlDto> createShortenedURL(@Valid @RequestBody final UrlDto payload) {
+    return ResponseEntity.ok(payload);
   }
 }
---
+++ src/main/java/com/showtime/analytics/codingchallenge/dto/UrlDto.java
@@ -0,0 +1,26 @@
+package com.showtime.analytics.codingchallenge.dto;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Builder;
+import lombok.Value;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+@Value
+@Builder(builderClassName = "Builder")
+@JsonDeserialize(builder = UrlDto.Builder.class)
+public class UrlDto {
+
+  @NotNull
+  String fqdn;
+
+  String shortenedUrl;
+
+  @JsonPOJOBuilder(withPrefix = StringUtils.EMPTY)
+  public static class Builder {
+  }
+}
